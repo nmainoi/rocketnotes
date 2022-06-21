31bb870bffd36927ce79d51609be4af968f05435
Maino Estivalet <nmainoi@hotmail.com>
2022-06-21 11:48:28 -0300

    span example

---
+++ src/components/Tag/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './style'
+
+export function Tag({ title }) {
+
+    return (
+        <Container>
+            {title}
+        </Container>
+    )
+}
--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,12 +1,26 @@
-import { Container } from "./style.js"
+import { Container, Links } from "./style.js"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
+
 export function Details() {
 
 
   return (
     <Container>
       <Header />
+      <Section title="Links uteis">
+        <Links>
+          <li><a href="">abacate</a></li>
+          <li><a href="">abacate 2</a></li>
+
+        </Links>
+      </Section>
+      <Section title="Marcadores">
+          <Tag title="express"/>
+          <Tag title="node"/>
+      </Section>
       <Button title="voltar"></Button>
     </Container>
   )
