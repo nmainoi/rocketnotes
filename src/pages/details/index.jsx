67e09fd67406be49739406beb20ca9c8c8d6e7a9
Maino Estivalet <nmainoi@hotmail.com>
2022-06-21 12:19:52 -0300

    details

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,6 +1,7 @@
-import { Container, Links } from "./style.js"
+import { Container, Links, Content } from "./style.js"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { ButtonText } from "../../components/ButtonText"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
 
@@ -9,19 +10,27 @@ export function Details() {
 
   return (
     <Container>
-      <Header />
-      <Section title="Links uteis">
-        <Links>
-          <li><a href="">abacate</a></li>
-          <li><a href="">abacate 2</a></li>
+                <Header />
+      <main>
+        <Content>
 
-        </Links>
-      </Section>
-      <Section title="Marcadores">
-          <Tag title="express"/>
-          <Tag title="node"/>
-      </Section>
-      <Button title="voltar"></Button>
+          <ButtonText title="Excluir a nota" />
+
+          <h1>Introducao ao react</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque mollitia ut aspernatur aliquid, suscipit, sit eveniet unde ratione delectus veritatis cum molestiae dolorem debitis! Beatae dignissimos non soluta maxime.</p>
+          <Section title="Links uteis">
+            <Links>
+              <li><a href="">abacate</a></li>
+              <li><a href="">abacate 2</a></li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+          <Button title="voltar"></Button>
+        </Content>
+      </main>
     </Container>
   )
 }
--- src/pages/details/style.js
+++ src/pages/details/style.js
@@ -3,12 +3,18 @@ import styled from 'styled-components';
 export const Container = styled.div`
 width: 100%;
 height: 100vh;
-display: grid;
 
+display: grid;
 grid-template-rows: 105px auto;
 grid-template-areas:
 "header"
 "content";
+
+> main{
+    grid-area: content;
+    overflow-y: scroll;
+    padding: 64px 0;
+}
 `
 
 export const Links = styled.ul`
@@ -24,4 +30,28 @@ export const Links = styled.ul`
 
     }
 
+`;
+
+export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    > button:first-child{
+        align-self: flex-end;
+    }
+
+    > h1{
+        font-size: 36px;
+        font-weight: 500;
+        padding-top: 64px;
+    }
+
+    > p{
+        font-size: 16px;
+        margin-top: 16px;
+        text-align: justify;
+    }
 `;
