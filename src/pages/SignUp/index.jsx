c559dcf48024e7a034cf030b62355f731a9dd361
Maino Estivalet <nmainoi@hotmail.com>
2022-06-23 18:33:50 -0300

    SignUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -4,17 +4,23 @@ import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
-export function SignIn() {
+import { FiLogIn, FiUser, FiLock } from 'react-icons/fi'
+export function SignUp() {
 
 
     return (
         <Container>
+                 <Background/>
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>Aplicacao para salvar e gerenciar seus links uteis.</p>
 
-                <h2>Faca seu login</h2>
+                <h2>Crie sua conta</h2>
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
                 <Input
                     placeholder="E-mail"
                     type="text"
@@ -25,10 +31,10 @@ export function SignIn() {
                     type="password"
                     icon={FiLock}
                 />
-                <Button title='Entrar'/>
-                <a href='#'> Criar conta</a>
+                <Button title='Cadastrar'/>
+                <a href='#'>Voltar para o login</a>
             </Form>
-            <Background/>
+       
         </Container>
     )
 }
