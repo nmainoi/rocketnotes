1ba1253fdb49a4f9618f8d74d66c2f7dd2a2fdb7
Maino Estivalet <nmainoi@hotmail.com>
2022-06-26 15:54:08 -0300

    NoteItem

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,54 @@
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { TextArea } from '../../components/TextArea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+
+
+
+
+
+import { Container, Form } from './style';
+
+export function New() {
+
+    return (
+        <Container>
+            <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a hrf="/"> voltar</a>
+                    </header>
+                    <Input placeholder="Titulo" />
+                    <TextArea placeholder="Observacoes" />
+
+                    <Section title="Links uteis">
+                        <NoteItem
+                            value="https://www.google.com"
+                        />
+                        <NoteItem
+                            isNew
+                            placeholder="Novo link"
+                        />
+                    </Section>
+                    <Section title="Marcadores">
+                        <div className='tags'>
+                        <NoteItem
+                            value="react"
+                            />
+                        <NoteItem
+                            isNew
+                            placeholder="Nova tag"
+                            /></div>
+                    </Section>
+                    <Button title="salvar"/>
+                </Form>
+
+            </main>
+        </Container>
+    )
+}
