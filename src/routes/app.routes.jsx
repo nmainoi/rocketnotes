e55e824b031db84e90bfb0b24ae5ba407b36a459
Maino Estivalet <nmainoi@hotmail.com>
2022-06-26 16:07:43 -0300

    routes init

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route} from 'react-router-dom';
+
+import { New } from '../pages/New';
+import { Home } from '../pages/Home';
+import { Profile } from '../pages/Profile';
+import { Details } from '../pages/Details';
+
+export function AppRoutes(){
+
+    return(
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/New" element={<New/>} />
+            <Route path="/Profile" element={<Profile/>} />
+            <Route path="/Details/:id" element={<Details/>} />
+        </Routes>
+    )
+}
